2972b8c71a4d3f0790c96e1f80004d083c13265d
Marina Melo <marinampires@gmail.com>
2016-02-10 22:39:53 -0200

    show current date in time zone America/Sao_Paulo

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	@now = Time.current.in_time_zone("America/Sao_Paulo").strftime("%d/%m/%Y %H:%M:%S")
   end
 end
