8db90c64ce44f2dd110f3c2431f421082431906b
Marina Melo <marinampires@gmail.com>
2016-02-13 21:10:27 -0200

    add the cart model and add item (product) into a cart

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+	if session[:counter].nil?
+		session[:counter] = 0
+	end
+
+	session[:counter] += 1
+
+	@counter = session[:counter]
   	@products = Product.order(:title)
   	@now = Time.current.in_time_zone("America/Sao_Paulo").strftime("%d/%m/%Y %H:%M:%S")
   end
